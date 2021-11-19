d1a2cf8bfcdb408cddaed95a115508b4024bef13
Supratim Nag <supratimnag1997@gmail.com>
2021-11-19 17:50:08 +0530

    --addnig simple hover effects

--- src/components/util/NavigateTo.jsx
+++ src/components/util/NavigateTo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import classes from './NavigateTo.module.css';
+
 const NavigateTo = (props) => {
   const navigate = useNavigate();
   return (
     <div
-      className={props.className}
+      className={`${props.className} ${classes.navTo}`}
       onClick={() => navigate(`/blogs/${props.id}`)}
     >
       {props.children}
